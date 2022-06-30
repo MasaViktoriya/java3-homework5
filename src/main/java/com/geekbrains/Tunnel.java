55a16a2bcf374be39cd9f523ec0e68ccc6089669
MasaViktoriya <viktoriya.masa@gmail.com>
2022-07-01 00:59:24 +0300

    ДЗ 5. Гонка пофикшена

--- src/main/java/com/geekbrains/Tunnel.java
+++ src/main/java/com/geekbrains/Tunnel.java
@@ -11,12 +11,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                Main.semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000L);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                Main.semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
